cab64fdfbe5eb9e48c103ff01a72ac74fb5822d1
VictorFer25 <47438634+VictorFer25@users.noreply.github.com>
2025-11-12 19:05:04 -0500

    chore(auth): disable middleware auth

--- app/middleware/auth-middleware.global.ts
+++ app/middleware/auth-middleware.global.ts
@@ -2,12 +2,12 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
   const auth = useAuthStore();
 
-  if (to.path !== '/login' && !auth.isAuthenticated) {
-    return navigateTo('/login');
-  }
+  // if (to.path !== '/login' && !auth.isAuthenticated) {
+  //   return navigateTo('/login');
+  // }
 
-  if (to.path === '/login' && auth.isAuthenticated) {
-    return navigateTo('/');
-  }
+  // if (to.path === '/login' && auth.isAuthenticated) {
+  //   return navigateTo('/');
+  // }
  
 });
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,20 +1,20 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
 
-  try {
-      const res = await fetchWithAuth<{
-        id: number,
-        username: string,
-        name: string,
-        last_name: string,
-        remember_me: boolean
-     }>(event, `/auth/me`);
+  // try {
+  //   //   const res = await fetchWithAuth<{
+  //   //     id: number,
+  //   //     username: string,
+  //   //     name: string,
+  //   //     last_name: string,
+  //   //     remember_me: boolean
+  //   //  }>(event, `/auth/me`);
 
-     event.context.user = res;
-     event.context.authenticated = true;
-    } catch (error) {
-      deleteCookie(event, 'access_token');
-    } finally {
-      return;
-    }
+  //    event.context.user = res;
+  //    event.context.authenticated = true;
+  //   } catch (error) {
+  //     deleteCookie(event, 'access_token');
+  //   } finally {
+  //     return;
+  //   }
 })
