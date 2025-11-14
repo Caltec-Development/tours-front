7d6ba475d98d6fac14c73336a7ba5064c175daf0
VictorFer25 <47438634+VictorFer25@users.noreply.github.com>
2025-11-13 19:20:00 -0500

    86dydh3v4-create-index-table-person-type (#1)

---
+++ app/pages/catalogs/person-types/index.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui';
+import type { PersonType } from '~/interfaces/person-type';
+
+const openCreateModal = ref<boolean>(false);
+
+const columns: TableColumn<PersonType>[] = [
+    {
+        accessorKey: 'id',
+        header: $t('catalogs.personType.table.columns.id'),
+    },
+    {
+        accessorKey: 'code',
+        header: $t('catalogs.personType.table.columns.code'),
+    },
+    {
+        accessorKey: 'name',
+        header: $t('catalogs.personType.table.columns.name'),
+    },
+    {
+        accessorKey: 'actions',
+        header: $t('products.table.columns.actions'),
+    }
+
+];
+
+const tableRef = ref<InstanceType<typeof import('~/components/ServerSideTable.vue').default> | null>(null);
+
+
+
+</script>
+
+<template>
+    <div class="space-y-6">
+        <div class="flex items-center justify-between">
+            <div>
+                <h1 class="text-2xl font-bold text-gray-900 dark:text-white">
+                    {{ $t('catalogs.personType.title') }}
+                </h1>
+            </div>
+            <UButton color="primary" size="lg" @click="openCreateModal = true">
+                <template #leading>
+                    <UIcon name="i-heroicons-plus" />
+                </template>
+                {{ $t('catalogs.personType.addNew') }}
+            </UButton>
+        </div>
+        <ServerSideTable
+            ref="tableRef"
+            :columns="columns"
+            :pageSizeOptions="[50, 100, 200]"
+            :initialPageSize="100"
+            url="/api/catalogs/person-type"
+        >
+            <template #actions-cell="{ row }">
+            </template>
+
+        </ServerSideTable>
+    </div>
+</template>
